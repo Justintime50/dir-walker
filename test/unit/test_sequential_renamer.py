22657dbdb6b8b61b63e83b0e78e5e67e5ee52915
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-09-17 00:18:57 -0600

    fix: correct tests

--- test/unit/test_sequential_renamer.py
+++ test/unit/test_sequential_renamer.py
@@ -3,14 +3,14 @@ from roverio import sequential_renamer
 
 
 def test_rename_files_no_force():
-    messages, files_updated = sequential_renamer.SequentialRenamer.rename_files('assets', force=False)
-    assert 'assets/showcase.png  ->  assets-0.png' in messages
-    assert files_updated == 1
+    messages, files_updated = sequential_renamer.SequentialRenamer.rename_files('.github/workflows', force=False)
+    assert '.github/workflows/build.yml  ->  workflows-1.yml' in messages
+    assert files_updated == 2
 
 
 @mock.patch('os.rename')
 def test_rename_files_force(mock_rename):
-    messages, files_updated = sequential_renamer.SequentialRenamer.rename_files('assets', force=True)
+    messages, files_updated = sequential_renamer.SequentialRenamer.rename_files('.github/workflows', force=True)
     assert mock_rename.called_once()
-    assert 'assets/showcase.png  ->  assets-0.png' in messages
-    assert files_updated == 1
+    assert '.github/workflows/build.yml  ->  workflows-1.yml' in messages
+    assert files_updated == 2
